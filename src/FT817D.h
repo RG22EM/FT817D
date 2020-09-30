812f591bf9232a93015f0c6d180c2e79cc77dacb
RG22EM <admin@orviss.ru>
2020-09-30 23:18:51 +0300

    Update FT817D.h

--- src/FT817D.h
+++ src/FT817D.h
@@ -1,8 +1,21 @@
 /*Библиотека реализующая функции трансивера FT-817 / FT-857 /FT-897 
 *
 */
-
 #ifndef FT817_h
 #define FT817_h
+
+
+#include "Arduino.h"
+
+class FT817
+{
+  public:
+    FT817(int pin);
+    void dot();
+    void dash();
+  private:
+    int _pin;
+};
+ 
 #endif
 
