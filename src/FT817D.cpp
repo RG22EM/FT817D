a190c41225b3f3f4a771e93f8ce3389a52ddf4b4
RG22EM <admin@orviss.ru>
2020-09-30 23:21:36 +0300

    Update FT817D.cpp

--- src/FT817D.cpp
+++ src/FT817D.cpp
@@ -1,1 +1,24 @@
-
+#include "Arduino.h"
+#include "FT817.h"
+ 
+FT817::FT817(int pin)
+{
+  pinMode(pin, OUTPUT);
+  _pin = pin;
+}
+ 
+void FT817::dot()
+{
+  digitalWrite(_pin, HIGH);
+  delay(250);
+  digitalWrite(_pin, LOW);
+  delay(250);  
+}
+ 
+void FT817::dash()
+{
+  digitalWrite(_pin, HIGH);
+  delay(1000);
+  digitalWrite(_pin, LOW);
+  delay(250);
+}
